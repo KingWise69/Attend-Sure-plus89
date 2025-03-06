b0d002812cb0fed8d5340da8bae7f01693764ce7
Magezi Joshua Wisdom <classicjoshus@gmail.com>
2025-03-06 14:00:14 +0300

    web-build

---
+++ app.js
@@ -0,0 +1,10 @@
+// Function to handle the navigation (similar to React Native navigation)
+function navigateToLogin(role) {
+    if (role === 'student') {
+        // Replace with actual navigation logic
+        window.location.href = "loginp.html"; // Navigate to the Student Login page
+    } else if (role === 'teacher') {
+        // Replace with actual navigation logic
+        window.location.href = "logint.html"; // Navigate to the Teacher Login page
+    }
+}
